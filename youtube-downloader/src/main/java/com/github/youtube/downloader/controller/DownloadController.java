6de28bba46ad8f171d46cdb4e2c2924b57fdd0a2
Anushka <anushka.mcg@gmail.com>
2020-06-27 11:44:39 +0530

    allowed cors

--- youtube-downloader/src/main/java/com/github/youtube/downloader/controller/DownloadController.java
+++ youtube-downloader/src/main/java/com/github/youtube/downloader/controller/DownloadController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,6 +30,7 @@ import com.github.youtube.downloader.service.DownloadService;
 import com.github.youtube.downloader.service.FileTypes;
 
 @Controller
+@CrossOrigin(origins = "${client.url}")
 public class DownloadController {
 
 	@Autowired
